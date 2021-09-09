a9c0d90573f6ee71908edb99e229e60ec4cf9565
jamesonion <jamesqnguyen01@gmail.com>
2021-09-09 16:42:53 -0400

    Added avatar profile pic
    Issues:
    - trying to use interpolation to display username rather than "Profile"
    - avatar image does not show based on the class' profilePic variable

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./profile.css";
+import Avatar from "@material-ui/core/Avatar";
 import { Link } from "react-router-dom";
-
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "user12039812093",
+            profilePic: "https://gooddoggies.online/wp-content/uploads/2020/06/5-Tips-To-Training-A-Labrador-Puppy-1.jpg.",
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,7 +25,9 @@ class Profile extends React.Component {
     render() {
         return(
             <div className="profile">
-                <h1> Profile </h1>
+                <h1> {this.username} Username </h1>
+                <Avatar className="profile__picture" src={this.profilePic} />
+
                 <form onSubmit={this.handleSubmit}>
                     <span> 
                         <h3> Username: <input type="text" value={this.state.username} onChange={this.handleChange} /> </h3>
