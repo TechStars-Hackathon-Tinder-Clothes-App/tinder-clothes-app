3d8a35b3a6d0a782430e55b9336d200ef7bd0646
jamesonion <jamesqnguyen01@gmail.com>
2021-09-10 10:24:10 -0400

    Fixed Profile Picture Sizing and Username
    Issues:
    - textfield handles onChange so Save button does not do anything substantial

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./profile.css";
 import Avatar from "@material-ui/core/Avatar";
+import "./Material UI components/Textfield"
 import { Link } from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 class Profile extends React.Component {
     constructor(props) {
@@ -13,7 +17,7 @@ class Profile extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    
     handleChange(event) {
         this.setState({username: event.target.value});
     }
@@ -25,21 +29,42 @@ class Profile extends React.Component {
     render() {
         return(
             <div className="profile">
-                <h1> {this.props.username} Username </h1>
+                <h1> {this.state.username} </h1>
                 <Avatar className="profile__picture" 
-                src={this.props.profilePicUrl} 
+                src={this.state.profilePicUrl}
+                style = {{
+                    height:'15vh',
+                    width: '15vh'
+                }}
                 />
-                <form onSubmit={this.handleSubmit}>
-                    <span> 
-                        <h3> Username: 
-                            <input type="text" value={this.state.username} onChange={this.handleChange} />
-                            <input type="submit" value="Submit" />
-                        </h3>
+                    <span>
+                        <TextField id="standard-basic" label="Username" value={this.state.username} onChange={this.handleChange}/>
+                        <Button
+                        variant="contained"
+                        onClick={this.handleSubmit}
+                        disableElevation
+                        >
+                            Save
+                        </Button>
                     </span>
-                </form>
             </div>
         )
     }
 }
 
 export default Profile;
+
+
+// const [people, setPeople] = useState([]);
+
+// useEffect(() => {
+//   const unsubscribe = database
+//     .collection("people")
+//     .onSnapshot((snapshot) =>
+//       setPeople(snapshot.docs.map((doc) => doc.data()))
+//     );
+
+//   return () => {
+//     unsubscribe();
+//   };
+// }, []
