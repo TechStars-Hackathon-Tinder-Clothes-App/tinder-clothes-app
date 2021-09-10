60ccde786079f7647588c75022e1d34f9dee9018
jamesonion <jamesqnguyen01@gmail.com>
2021-09-10 11:09:12 -0400

    Wardrobe WIP
    Issues:
    - add button to "upload" more pictures
    - allow for tapping / swiping to see other clothes in profile

--- src/Wardrobe.js
+++ src/Wardrobe.js
@@ -19,23 +19,54 @@ function Wardrobe() {
   }, []);
 
   return (
-
     <div>
       <div className="tinderCards__cardContainer">
-        {people.map((person) => (
-          <TinderCard
-            className="swipe"
-            key={person.name}
-            preventSwipe={["up", "down"]}
-          >
-            <div
-              style={{ backgroundImage: `url(${person.url})` }}
-              className="card"
-            >
-              <h3>{person.name}</h3>
-            </div>
-          </TinderCard>
-        ))}
+        {people.map((person) => {
+          if (person.userId === 0) {
+            return (
+              <TinderCard
+                key={person.name}
+              >
+                <div
+                  style={{
+                    backgroundImage: `url(${person.url})`,
+                    overflowY: "scroll",
+                    overflowWrap: "break-word",
+                  }}
+                  className="card"
+                >
+                  <h3>{person.name}</h3>
+                  <div
+                    style={{
+                      position: `absolute`,
+                      paddingBottom: `5%`,
+                      bottom: `-60%`,
+                    }}
+                  >
+                    <p className="row">
+                      <h4 style={{ display: "inline-block" }}>Description:</h4>
+                      <p style={{ display: "block" }}>{person.description}</p>
+                    </p>
+                    <p className="row">
+                      <h4 style={{ display: "inline-block" }}>Brand:</h4>
+                      <p style={{ display: "block" }}>{person.brand}</p>
+                    </p>
+                    <p className="row">
+                      <h4 style={{ display: "inline-block" }}>Size:</h4>
+                      <p style={{ display: "block" }}>{person.size}</p>
+                    </p>
+                    <p className="row">
+                      <h4 style={{ display: "inline-block" }}>Condition:</h4>
+                      <p style={{ display: "block" }}>{person.condition}</p>
+                    </p>
+                  </div>
+                </div>
+              </TinderCard>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     </div>
   );
